08398a4e8e7082318276376fe855823a23456bd6
pollas1234 <1111112eje@gmail.com>
2025-11-08 21:19:36 +0100

    Actualizar pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.play(music.createSoundEffect(WaveShape.Noise, 4292, 1400, 255, 0, 150, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    grid.move(mySprite, 1, 0)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.play(music.createSoundEffect(WaveShape.Noise, 3300, 1400, 255, 0, 150, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    grid.move(mySprite, -1, 0)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (grid.spriteCol(mySprite) == 0) {
+        calculator.Add(game.askForNumber(""), game.askForNumber(""))
+    } else if (grid.spriteCol(mySprite) == 1) {
+        calculator.Subtract(game.askForNumber(""), game.askForNumber(""))
+    } else if (grid.spriteCol(mySprite) == 2) {
+        calculator.Multiply(game.askForNumber(""), game.askForNumber(""))
+    } else if (grid.spriteCol(mySprite) == 3) {
+        calculator.Divide(game.askForNumber(""), game.askForNumber(""))
+    } else if (grid.spriteCol(mySprite) == 4) {
+        calculator.Exponentiate(game.askForNumber(""), game.askForNumber(""))
+    } else {
+        calculator.Factorial(game.askForNumber(""))
+    }
+})
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`nivel`)
+mySprite = sprites.create(img`
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    `, SpriteKind.Player)
+grid.moveWithButtons(mySprite)
+tiles.placeOnRandomTile(mySprite, assets.tile`miMosaico`)
---
+++ tilemap.g.ts
@@ -0,0 +1,48 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "nivel":
+            case "nivel1":return tiles.createTilemap(hex`06000100010203040506`, img`
+. . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "miMosaico3":
+            case "tile5":return tile5;
+            case "miMosaico":
+            case "tile1":return tile1;
+            case "miMosaico0":
+            case "tile2":return tile2;
+            case "miMosaico1":
+            case "tile3":return tile3;
+            case "miMosaico2":
+            case "tile4":return tile4;
+            case "miMosaico4":
+            case "tile6":return tile6;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
